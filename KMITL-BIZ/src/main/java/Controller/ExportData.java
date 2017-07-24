5d4bc35dd38695c587c83448ed556ee131e96205
Sanhaphat Srijumpa <fluke031@hotmail.com>
2017-07-24 18:39:36 +0700

    Revert "Change package location"
    This reverts commit 30e4be168c572a60d6d56a1233bac16927ed8eb4.

--- KMITL-BIZ/src/main/java/Controller/ExportData.java
+++ KMITL-BIZ/src/main/java/Controller/ExportData.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Controller;
+package Servlet;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
